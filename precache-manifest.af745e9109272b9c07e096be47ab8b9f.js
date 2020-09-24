b8380be0bc9b47230738d6dd354d9ee447008865
AdarshPatel <adarshzpatel@gmail.com>
2020-09-24 18:36:56 +0530

    Updates

---
+++ precache-manifest.af745e9109272b9c07e096be47ab8b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a09ffb315991f30e941a83346aab83f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6dd5dd97a1e8bff40fd3",
+    "url": "/robofriends/static/css/main.d5129dca.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd5dd97a1e8bff40fd3",
+    "url": "/robofriends/static/js/main.ba9a55a2.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
